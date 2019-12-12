566a899be29f4a5ec744be3458f6b68938054e79
Antoine <arosenbach@salesforce.com>
2019-12-12 16:40:59 +0100

    refactor: introduce the per file command pattern

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-const program = require("commander");
+const program = require('commander'),
+  status = require('./src/commands/status/status');
 
-program.command("status").action(() => {
-  console.log("No pending contracts.");
-});
+program.command('status').action(() => console.log(status().message));
 program.parse(process.argv);
