5dd4c8ab2a71eb0a2d63bf048f3f792c1fa9b59d
rami al daher noufal <rnoufal@salesforce.com>
2020-04-27 12:40:15 +0200

    View no pending changes, fixing issues
    Resolving two issues
     - Usage: [object Object] status [options]
     - install:global script doesn't work properly
    
    Updating readme file
    
    Changing work item status in the sprint to ready for review
    
    Changing work item status in the sprint to ready for review

--- src/main/cli/cli-app.ts
+++ src/main/cli/cli-app.ts
@@ -4,6 +4,8 @@ import {Command} from 'commander';
 import {StatusCommand} from './commands/status-command';
 import {AvocatCommand} from './commands/avocat-command';
 import {Inject, Service} from 'typedi';
+import chalk from 'chalk';
+import figlet from 'figlet';
 
 @Service()
 export class CliApp {
@@ -25,8 +27,9 @@ export class CliApp {
         }
     }
 
-    private initMainCommand(version: string): void{
+    private initMainCommand(version: string): void {
         this.mainCommand
+            .name('avocat')
             .description('🥑 Continuous contract testing for HTTP APIs')
             .version(version, '-v, --version', 'Avocat current version')
             .helpOption('-h, --help', 'Show available commands/options')
@@ -42,7 +45,16 @@ export class CliApp {
     }
 
     private printDefaultHelpMessage(): void {
+        CliApp.drawAvocatBanner();
         this.mainCommand.outputHelp();
+    }
+
+    private static drawAvocatBanner(): void {
+        console.log(
+            chalk.greenBright(
+                figlet.textSync('Avocat', {horizontalLayout: 'full'})
+            )
+        );
     }
 
     private static noProvidedArguments(): boolean {
--- src/main/index.ts
+++ src/main/index.ts
@@ -2,18 +2,7 @@
 
 import {CliApp} from './cli/cli-app';
 import {version} from '../../package.json';
-
-import chalk from 'chalk';
-import clear from 'clear';
-import figlet from 'figlet';
 import {Container} from 'typedi';
-
-clear(); // clear console every time
-console.log(
-    chalk.red(
-        figlet.textSync('Avocat', {horizontalLayout: 'full'}) // print avocat banner
-    )
-);
 
 const app = Container.get(CliApp);
 app.run(process.argv, version);
