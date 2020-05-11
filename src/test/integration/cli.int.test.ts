01ffd8c66ddf1c13ee2f7b3976509ca4fdd98787
Rami Noufal <rnoufal@salesforce.com>
2020-05-11 14:49:19 +0200

    [debt 🦟] Snapshot tests fail on Jenkins (#23)
    * Refactoring jest timeout custom config (3000ms) to be used once in a file within the (beforeAll) and checking that it doesn't affect default jest timeout (5000ms) for other test modules
    
    * Checks are failing in the Pipeline, trying to remove 'Publish unit test & coverage reports' stage

--- src/test/integration/cli.int.test.ts
+++ src/test/integration/cli.int.test.ts
@@ -1,10 +1,15 @@
 import {TestCliUtils} from '../utils/test-cli-utils';
 
 describe('CLI Test', () => {
+
+    beforeAll(() => {
+        const INTEGRATION_TEST_TIMEOUT = 30000;
+        jest.setTimeout(INTEGRATION_TEST_TIMEOUT);
+    });
+
     describe('Help Command Test', () => {
         describe('When running avocat with help option', () => {
             it('Shows available options and commands', () => {
-                jest.setTimeout(30000);
                 return TestCliUtils.transpileAndRunAvocatFromCLI(['--help']).then(result => {
                     expect(result.code).toBe(0);
                     expect(result.stdout.trim()).toMatchSnapshot();
--- src/test/utils/test-cli-utils.unit.test.ts
+++ src/test/utils/test-cli-utils.unit.test.ts
@@ -1,30 +1,38 @@
 import {TestCliUtils} from './test-cli-utils';
 import {Command} from 'commander';
-const TIMEOUT = 30000;
+
 describe('Test CLI Utils', () => {
+
+    beforeAll(() => {
+        const INTEGRATION_TEST_TIMEOUT = 30000;
+        jest.setTimeout(INTEGRATION_TEST_TIMEOUT);
+    });
+
     describe('When the command is executed successfully', () => {
         it('Should return stdout with code 0', async () => {
             const result = await TestCliUtils.transpileAndRunAvocatFromCLI(['node']);
+
             expect(result).not.toBeUndefined();
             expect(result.code).toBe(0);
-        }, TIMEOUT);
+        });
     });
 
     describe('When dist/index could not be found', () => {
         it('Should return code should not be 0', async () => {
             const result = await TestCliUtils.transpileAndRunAvocatFromCLI([], '../dummy');
+
             expect(result.code).not.toBe(0);
-        }, TIMEOUT);
+        });
     });
 
     describe('When runCommand function is called with (mainCommand and command)', () => {
-        it('Should call mainCommand.parse with the provided parameters', () => {
+        it('Should call mainCommand.parse with the provided parameters',() => {
             const mainCommand: Command = jest.genMockFromModule('commander');
             mainCommand.parse = jest.fn();
 
             TestCliUtils.runCommand(mainCommand, 'ls');
 
             expect(mainCommand.parse).toHaveBeenCalledTimes(1);
-        }, TIMEOUT);
+        });
     });
 });
