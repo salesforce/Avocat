4f6eec832719d03252af85644c51fbd9567885a5
rami al daher noufal <rnoufal@salesforce.com>
2020-07-29 10:27:56 +0200

    Adding "!" to the copyright comments, so tsc can transpile them Changing 2018 -> 2020 Removing extra wildcards characters "*"

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-/*
- *
- *  * Copyright (c) 2018, salesforce.com, inc.
- *  * All rights reserved.
- *  * SPDX-License-Identifier: BSD-3-Clause
- *  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- *
+/*!
+ * Copyright (c) 2020, Salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
 import CliApp from './app/cli/cli-app';
